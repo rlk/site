ae08393bc8fbfbdedfb330d984bdd1cf94407727
Robert Kooima <robert.kooima@gmail.com>
2014-09-29 13:41:58 -0500

    Remove unnecessary logging

--- courses/csc4356/notes/script.js
+++ courses/csc4356/notes/script.js
@@ -2,8 +2,6 @@ window.onkeydown = function(event) {
 	var prev = document.getElementById("prev");
 	var next = document.getElementById("next");
 
-	console.log(event);
-
 	switch (event.keyCode) {
 		case 37:
 			if (prev) {
