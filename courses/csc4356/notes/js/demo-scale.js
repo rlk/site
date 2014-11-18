cc03bbb440d669e8f1707d0ea578cd3f60c4608c
Robert Kooima <robert.kooima@gmail.com>
2014-11-18 15:03:06 -0600

    Testing demo rewrite for greater independence

--- courses/csc4356/notes/js/demo-scale.js
+++ courses/csc4356/notes/js/demo-scale.js
@@ -3,37 +3,53 @@
 // Requires demo-transformation.js
 
 function DemoScale() {
-    DemoTransform.call(this, "DemoScale");
+    DemoTransform.call(this, "demoScaleCanvas");
+
+    var demoScaleXIn  = document.getElementById("demoScaleXIn");
+    var demoScaleYIn  = document.getElementById("demoScaleYIn");
+    var demoScaleZIn  = document.getElementById("demoScaleZIn");
+
+    var demoScaleXOut = document.getElementById("demoScaleXOut");
+    var demoScaleYOut = document.getElementById("demoScaleYOut");
+    var demoScaleZOut = document.getElementById("demoScaleZOut");
+
+    var that = this;
+
+    function update() {
+
+        // Get the current (x, y, z) slider values.
+
+        var x = parseFloat(demoScaleXIn.value);
+        var y = parseFloat(demoScaleYIn.value);
+        var z = parseFloat(demoScaleZIn.value);
+
+        // Set the current numerical display.
+
+        demoScaleXOut.innerHTML = x.toFixed(1);
+        demoScaleYOut.innerHTML = y.toFixed(1);
+        demoScaleZOut.innerHTML = z.toFixed(1);
+
+        // Create a transformation matrix and set its uniform.
+
+        var M = mat4.create();
+
+        mat4.ortho(M, -1, +1, -1, +1, -1, +1);
+        mat4.scale(M, M, vec3.fromValues(x, y, z));
+
+        that.gl.uniformMatrix4fv(that.ModelLocation, false, M);
+
+        // Draw the updated frame.
+
+        that.gl.clear(that.gl.COLOR_BUFFER_BIT);
+        that.draw();
+    }
+
+    document.getElementById("demoScaleXIn").oninput = update;
+    document.getElementById("demoScaleYIn").oninput = update;
+    document.getElementById("demoScaleZIn").oninput = update;
 }
 
 DemoScale.prototype = Object.create(DemoTransform.prototype);
 DemoScale.prototype.constructor = DemoScale;
 
-DemoScale.prototype.update = function() {
-
-    // Get the current (x, y, z) slider values.
-
-    var x = parseFloat(document.getElementById("xin").value);
-    var y = parseFloat(document.getElementById("yin").value);
-    var z = parseFloat(document.getElementById("zin").value);
-
-    // Set the current numerical display.
-
-    document.getElementById("xout").innerHTML = x.toFixed(1);
-    document.getElementById("yout").innerHTML = y.toFixed(1);
-    document.getElementById("zout").innerHTML = z.toFixed(1);
-
-    // Create a transformation matrix and set its uniform.
-
-    var M = mat4.create();
-
-    mat4.ortho(M, -1, +1, -1, +1, -1, +1);
-    mat4.scale(M, M, vec3.fromValues(x, y, z));
-
-    this.gl.uniformMatrix4fv(this.ModelLocation, false, M);
-
-    // Draw the updated frame.
-
-    this.gl.clear(this.gl.COLOR_BUFFER_BIT);
-    this.draw();
-}
+demoScale = new DemoScale();
