2e1685c003dcf538654357f2badcca39e7c2f4e7
Robert Kooima <robert.kooima@gmail.com>
2025-07-13 15:22:58 -0500

    Fix CG course notes math.

---
+++ courses/csc4356/mathjax-config.js
@@ -0,0 +1,10 @@
+MathJax = {
+  tex: {
+    inlineMath: [['$', '$']],
+    macros: {
+      red: ['{\\color[rgb]{1.0,0.0,0.0} #1}', 1],
+      M: ['\\mathbf{#1}', 1],
+      V: ['\\mathbf{#1}', 1]
+    }
+  }
+};
